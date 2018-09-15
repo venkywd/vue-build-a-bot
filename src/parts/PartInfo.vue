c19e242d946862bacb5bcd93a6276d1282cb1776
John Papa <john@johnpapa.net>
2018-09-15 19:40:10 -0400

    route params and props

--- src/parts/PartInfo.vue
+++ src/parts/PartInfo.vue
@@ -6,15 +6,27 @@
 </template>
 
 <script>
+import parts from '../data/parts';
+
 export default {
   name: 'PartInfo',
-  data() {
-    return {
-      part: {
-        title: 'Part Title',
-        description: 'Part Description',
+  props: {
+    partType: { type: String },
+    id: {
+      type: [Number, String],
+      validator(value) {
+        return Number.isInteger(Number(value));
       },
-    };
+    },
+  },
+  computed: {
+    part() {
+      const { partType, id } = this; //this.$route.params;
+      return parts[partType].find(part => part.id === parseInt(id, 10));
+    },
+    data() {
+      return {};
+    },
   },
 };
 </script>
