1561d887268e747f2ffff92f469a9e717768dc8a
John Papa <john@johnpapa.net>
2018-09-15 22:29:46 -0400

    directive

--- src/build/PartSelector.vue
+++ src/build/PartSelector.vue
@@ -3,11 +3,13 @@
     <img :src="selectedPart.src" title="arm" @click="showPartInfo()" />
     <button @click="selectPreviousPart()" class="prev-selector"></button>
     <button @click="selectNextPart()" class="next-selector"></button>
-    <span class="sale" v-show="selectedPart.onSale">Sale!</span>
+    <span v-pin class="sale" v-show="selectedPart.onSale">Sale!</span>
   </div>
 </template>
 
 <script>
+import pinDirective from '../shared/pin-directive';
+
 function getPreviousValidIndex(index, length) {
   const deprecatedIndex = index - 1;
   return deprecatedIndex < 0 ? length - 1 : deprecatedIndex;
@@ -29,6 +31,7 @@ export default {
       },
     },
   },
+  directives: { pin: pinDirective },
   data() {
     return { selectedPartIndex: 0 };
   },
@@ -80,9 +83,6 @@ export default {
   border: 3px solid #aaa;
 }
 .sale {
-  position: absolute;
-  bottom: 5px;
-  right: 5px;
   color: white;
   background-color: red;
   padding: 3px;
